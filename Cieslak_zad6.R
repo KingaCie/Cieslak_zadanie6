12de949cadb7f690ac85c7de1cf95b7afc01cfa0
KingaCie <kinga.ci@wp.pl>
2021-03-24 19:35:01 +0100

    rozwiązania do zadan z rozdzialu 6

---
+++ Cieslak_zad6.R
@@ -0,0 +1,44 @@
+# ZADANIE 1
+library(stringr)
+t1 = "TERYT 18; podkarpackie; Rzeszów; 0.2 He; A"
+t2 = "TERYT 22; pomorskie;   Gdańsk; 12 C ; B"
+
+str_sub(t2, start = -5, end = -4)
+
+# ZADANIE 2
+
+horoskop = function(imie, miesiac){
+  if (miesiac%%2==0){
+    cat(paste("Osoba o imieniu: ", imie, "będzie miała jutro szczęście"))
+  }
+  else {
+    cat(paste("Osoba o imieniu: ", imie, "będzie miała jutro nieszczęście"))
+  }
+}
+
+# ZADANIE 3
+horoskop2 = function(imie, miesiac){
+  if ((str_sub(imie, start = 1, end = 1))=="K" | (str_sub(imie, start = 1, end = 1))=="M" | (str_sub(imie, start = 1, end = 1))=="Z"){
+    cat(paste("Osoba o imieniu: ", imie, "będzie miała jutro szczęście"))
+  }
+  else if (miesiac%%2==0){
+    cat(paste("Osoba o imieniu: ", imie, "będzie miała jutro szczęście"))
+  }
+  else {
+    cat(paste("Osoba o imieniu: ", imie, "będzie miała jutro nieszczęście"))
+  }
+}
+
+# ZADANIE 4
+t3 = "2019-03-11: 23.5, 19/03/12: 12.7, 2019.03.13: 11.1, 2019-marzec-14: 14.3"
+#daty
+str_extract_all(t3, pattern = "[0-9](\\-|\\.)[a-zA-Z0-9]+(\\-|\\.)[0-9]+")
+
+# ZADANIE 5
+wartosci2 = str_extract_all(t3, pattern = "\\:\\s[0-9]+\\.[0-9]")
+wartosci = str_extract_all(wartosci2, pattern = "[0-9]+\\.[0-9]")
+wartosci
+
+# ZADANIE 6
+wsp = c("52°24′N 16°55′E", "53°08′07″N 23°08′44″E", "39°6′N 84°31′W")
+str_extract_all(wsp, pattern = "[0-9]+\\°")
